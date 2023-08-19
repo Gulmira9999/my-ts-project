c5a6cbdd55eb469b5ff07aa31da28f06f5f55d81
Gulmira9999 <mammadovagulmira077@gmail.com>
2023-08-19 10:05:57 +0400

    sad

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from "react";
+import "./App.css";
+interface AppProps {}
+
+const App: FC<AppProps> = () => {
+  return (
+    <div className="App">
+      <header className="App-header">Learn React</header>
+    </div>
+  );
+};
+
+export default App;
