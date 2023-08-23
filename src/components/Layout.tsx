23643b962f46759e69fc0c35cd18b73f27259c1b
Gulmira9999 <mammadovagulmira077@gmail.com>
2023-08-23 19:31:13 +0400

    add genereteId

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid"; // Import the UUID v4 function
+
 const Layout = () => {
+  const generatedId = uuidv4();
+
   return (
     <ul>
       <li>
         <Link to="/signin">Sign in</Link>
       </li>
       <li>
-        <Link to={`/profiles`}>Profile</Link>
+        <Link to={`/profiles/${generatedId}`}>Profile</Link>
       </li>
     </ul>
   );
